00ee63923e78d85c3dec2280cd9dea4f3a191b0d
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-03 22:28:47 +0530

    frontend initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>Zerodha Project</h1>
+      <h1>Zerodha Project </h1>
     </div>
   </React.StrictMode>
 );
