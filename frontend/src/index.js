5c0e79e060e6b887cd2f2e42b40db90a1f7b06b8
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-03 22:32:32 +0530

    Set up project structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>Zerodha Project </h1>
+      <h1>Zerodha  Project </h1>
+      <h2>home</h2>
     </div>
   </React.StrictMode>
 );
