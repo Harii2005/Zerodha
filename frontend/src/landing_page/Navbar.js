b29b780922c49db1c70b4d599ec232f3eb8023f3
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-07 19:48:01 +0530

    footer

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,55 +2,45 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg border-bottom" style={{backgroundColor : "#fff"}}>
-
-    <div className="container-fluid p-2">
+    <nav className="navbar navbar-expand-lg border-bottom" style={{backgroundColor: "#fff"}}>
+      <div className="container">
         <a className="navbar-brand" href="#">
-        <img src='/assets/logo.svg' alt='logo' className="" style={{width : "17%"  , marginLeft : "160px"}}/>
+          <img src='/assets/logo.svg' alt='logo' style={{width: "130px"}}/>
         </a>
+        
         <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-        <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-
-        <form className="d-flex" role="search">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-                <a className="nav-link " aria-current="page" href="#">SignUp</a>
+              <a className="nav-link" aria-current="page" href="#">SignUp</a>
             </li>
-
             <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="#">About</a>
             </li>
-
             <li className="nav-item">
-                <a className="nav-link" href="#">Product</a>
+              <a className="nav-link" href="#">Product</a>
             </li>
-
             <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">Pricing</a>
             </li>
-
             <li className="nav-item">
-                <a className="nav-link" href="#">Support</a>
+              <a className="nav-link" href="#">Support</a>
             </li>
-
-        </ul>
-        </form>
+          </ul>
         </div>
-    </div>
+      </div>
     </nav>
-
   );
 }
 
