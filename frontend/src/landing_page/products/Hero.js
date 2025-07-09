5ac28e622144597d848378921b04dbd60d23445f
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-09 21:16:38 +0530

    added leftSection

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className='container mt-5'>
+            <div className='text-center  p-3'>
+                <h1 className='mt-5'>Zerodha Products</h1>
+                <p className='text-muted mt-3 fs-4'>Sleek, modern, and intuitive trading platforms</p>
+                <p className='text-muted mt-3' >Check out our <a href='' style={{textDecoration : "none"}}> investment offerings<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+            </div>
+        </div>
+
     );
 }
 
--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import Hero from './Hero';
-import LeftSection from './LeftSection';
-import RightSection from './RightSection';
-import Universe from './Universe';
-import OpenAccount from '../OpenAccount';
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
+import OpenAccount from "../OpenAccount";
 
 function ProductsPage() {
-    return (
-        <>
-            <Hero />
-            <LeftSection />
-            <RightSection />
-            <Universe />
-            <OpenAccount />
-        </>
-    );
+  return (
+    <>
+      <Hero />
+        <LeftSection
+            imageURL=" /assets/kite.png "
+            ProductName="Kite"
+            productDiscription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+            trydemo=""
+            learnmore=""
+            googleplay=""
+            appstore=""
+        />
+        <LeftSection
+            imageURL=" /assets/kite.png "
+            ProductName="Coin"
+            productDiscription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices.."
+            trydemo=""
+            learnmore=""
+            googleplay=""
+            appstore=""
+        />
+        <LeftSection
+            imageURL=" /assets/varsity.png "
+            ProductName="Varsity mobile"
+            productDiscription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+            trydemo=""
+            learnmore=""
+            googleplay=""
+            appstore=""
+        />
+
+      <RightSection />
+      <Universe />
+      <OpenAccount />
+    </>
+  );
 }
 
 export default ProductsPage;
