66c784404f6ede1de10a28f95ee41f62ebcf77b1
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-21 22:30:31 +0530

    added data

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,15 +8,36 @@ const Holdings = () => {
       <div className="order-table">
         <table>
           <tr>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg. cost</th>
-            <th>LTP</th>
-            <th>Cur. val</th>
-            <th>P&L</th>
-            <th>Net chg.</th>
-            <th>Day chg.</th>
+            <th> Instrument </th>
+            <th> Qty. </th>
+            <th> Avg. cost </th>
+            <th> LTP </th>
+            <th> Cur. val </th>
+            <th> P&L </th>
+            <th> Net chg. </th>
+            <th> Day chg. </th>
           </tr>
+
+          {holdings.map((stock , index) =>{
+            const currValue = stock.price * stock.qty;
+            const isProfit  = currValue - stock.avg - stock.qty >= 0.0 ;
+            const profClass = isProfit ? "profit"  : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+            return(
+              <tr key={index} >
+                <td>{stock.name}</td>                         
+                <td>{stock.name}</td>
+                <td>{stock.avg.toFixed(2)}</td>              
+                <td>{stock.price.toFixed(2)}</td>            
+                <td>{currValue.toFixed(2)}</td>               
+                <td className={profClass}>{(currValue - stock.avg * stock.qty).toFixed(2)}</td>                                        
+                <td className={profClass}>{stock.net}</td>   
+                <td className={dayClass}>{stock.day}</td>    
+              </tr>
+            )
+          })}
+
+
         </table>
       </div>
 
