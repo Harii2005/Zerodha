fd340c0027730803c65657b3224fa0b2c1c9150b
Harii2005 <harikrishnankrkv@gmail.com>
2025-07-25 21:06:11 +0530

    Schema and Model UPDATED

--- backend/schemas/PositionsSchema.js
+++ backend/schemas/PositionsSchema.js
@@ -1,13 +1,13 @@
 const { Schema } = require("mongoose");
 
 const PositionsSchema = new Schema({
-  name: String, // Stock symbol/name
-  qty: Number, // Quantity held
-  avg: Number, // Average buy price
-  price: Number, // Current market price
-  net: String, // Net P&L percentage
-  day: String, // Day's P&L percentage
-  isDown: Boolean, // True if position is in loss
+    name: String, // Stock symbol/name
+    qty: Number, // Quantity held
+    avg: Number, // Average buy price
+    price: Number, // Current market price
+    net: String, // Net P&L percentage
+    day: String, // Day's P&L percentage
+    isLoss: Boolean, // True if position is in loss
 });
 
 module.exports = { PositionsSchema };
